ef52ea53b2ed96de477e72aca06f3c403b818240
Ubuntu <ubuntu@ip-172-31-29-192.us-west-2.compute.internal>
2013-07-16 06:37:38 +0000

    FinalGrader

--- grader.js
+++ grader.js
@@ -65,10 +65,19 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'path to herokuapp url', clone(assertURLExists))       
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if(!program.url){ //original logic. file existing - index.html
+ var checkJson = checkHtmlFile(program.file, program.checks);
+ var outJson = JSON.stringify(checkJson, null, 4);
+ console.log("required.main if : \n" + outJson);
+ } else { // added
+// rest.get(program.url).on('complete', function(result){
+ var checkJson = checkURL(program.url, program.checks);
+ var outJson = JSON.stringify(checkJson, null, 4);
+ console.log("require.main else : \n" + outJson);
+ };
+
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
